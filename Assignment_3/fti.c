576aeddca5d4b646827d0a2cc659aeb259eff34e
zecarneiro <zecarneiro44@gmail.com>
2016-11-19 02:01:31 +0000

    Intro fti

---
+++ Assignment_3/fti.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include "funcs.h"
+
+#define PI 3.14159265359
+
+void fti(ImageF *in_re, ImageF *in_img, ImageF *out_re, ImageF *out_img, int inverse){
+	
+	if(inverse == 0){
+
+	}
+}
